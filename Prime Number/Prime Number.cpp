89787f5c29d28e3c226537bd37c1f6cda32ee34d
Mohammad-Mahdi1999 <44753085+Mohammad-Mahdi1999@users.noreply.github.com>
2018-11-08 19:00:59 +0330

    start

---
+++ Prime Number/Prime Number.cpp
@@ -0,0 +1,24 @@
+// In the name of God !
+//Prime Number
+//
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	int num, i,c=0;
+	cout << "Enter a number:";
+	cin >> num;
+	for (i = 1; i <= num; i++)
+		if (num%i == 0)
+			c++;
+	if (c == 2)
+		cout << "This is a Prime Number!" << endl;
+	else 
+		cout << "This is a Compound Number!" << endl;
+
+	cin.get();
+	cin.ignore();
+}
+//Created by /%++;; Mohammad-Mahdi Farah-bakhsh ;;++%\
